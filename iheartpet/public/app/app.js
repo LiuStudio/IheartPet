0f2f264c197f6d39a2df846c90413b182849319b
desongyu <desong.e.yu@gmail.com>
2016-07-24 21:31:17 -0400

    first connect angular, node, and mongo

---
+++ iheartpet/public/app/app.js
@@ -0,0 +1,4 @@
+(function(){
+    angular.module('IHeartPet', []);
+    console.log("In app.js")
+}());
--- iheartpet/routes/users.js
+++ iheartpet/routes/users.js
@@ -2,9 +2,15 @@ var express = require('express');
 var router = express.Router();
 //var users=require('users');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+    //db.users.find()
+   // res.sendfile('user.html')
+});
+
+/* GET users listing. */
+router.get('/:userid', function(req, res, next) {
+  res.send('respond with a resource' + req.params.userid);
     //db.users.find()
    // res.sendfile('user.html')
 });
